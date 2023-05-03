bf5ab057d1194d272621d180fd76c7c5c35a4883
Erhan Fırat <erhanfirat@gmail.com>
2023-05-03 13:33:25 +0300

    s7g3 - yup forms

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -1,30 +1,97 @@
 import axios from "axios";
-import { useState } from "react";
-import { Form, FormGroup, Input, Label, Button } from "reactstrap";
+import { useState, useEffect } from "react";
+import {
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Button,
+  FormFeedback,
+} from "reactstrap";
 import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
 
+// component başlangıcı
 const CreateProduct = () => {
+  // 1 - Data Derfinition: state, local, prop
+  // ************************************************
+  const [isFormValid, setFormValid] = useState();
   const [product, setProduct] = useState({
     name: "",
     img: "",
     description: "",
     price: 0,
     stock: 0,
+    type: "",
+  });
+
+  const formSchema = Yup.object().shape({
+    name: Yup.string()
+      .required("Ürün isim alanı zorunludur!")
+      .min(6, "Ürün ismi en az 6 karakter olmalıdır."),
+    img: Yup.string()
+      .min(6, "Görsel URL linki en az 6 karakter olmalıdır.")
+      .url("Lütfen geçerli bir URL bilgisi giriniz."),
+    description: Yup.string(),
+    price: Yup.number("Must be a number type") // Validates for numerical value
+      .typeError("Lütfen numeric data giriniz.")
+      .positive("Must be a positive value") // Validates against negative values
+      .required("Please enter a duration. The field cannot be left blank.") // Sets it as a compulsory field
+      .min(1, "Hey! Your duration must be greater than or equal to 1!"), // Sets a minimum value});,
+    stock: Yup.number("Lütfen numeric data giriniz!").min(
+      0,
+      "Stok bilgisi 0 dan az olamaz!"
+    ),
+    type: Yup.string(),
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    name: "",
+    img: "",
+    description: "",
+    price: "",
+    stock: "",
+    type: "",
   });
 
   const nav = useNavigate();
-  /**
-{
-  id: "11"
-  createdAt :  "2023-04-19T07:58:02.398Z"
-  //
-  name: "Violet Connelly"
-  img: "https://loremflickr.com/640/480/abstract"
-  description :  "Quasi delectus sint qui rerum optio. Iusto nobis omnis minus aliquid necessitatibu."
-  price: "259.00"
-  stock: "5"
-}
- */
+
+  // 2 - Methods, functions
+  // ************************************************
+
+  const inputChangeHandler = (e) => {
+    // destructuring
+    const { name, value } = e.target;
+
+    setProduct({ ...product, [name]: value });
+
+    // hata mesajhını yuptan alma
+    Yup.reach(formSchema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({ ...formErrors, [name]: "" });
+      })
+      .catch((err) => {
+        setFormErrors({ ...formErrors, [name]: err.errors[0] });
+      });
+  };
+
+  // 3 - Life cycle events: useEffect
+  // ************************************************
+
+  useEffect(() => {
+    formSchema.isValid(product).then((valid) => {
+      setFormValid(valid);
+    });
+  }, [product]);
+
+  useEffect(() => {
+    console.warn("formErrors: ", formErrors);
+  }, [formErrors]);
+
+  // 4 - templating
+  // ************************************************
+
   return (
     <div>
       <Form
@@ -47,11 +114,11 @@ const CreateProduct = () => {
             id="product-name"
             name="name"
             type="text"
-            onChange={(event) => {
-              setProduct({ ...product, name: event.target.value });
-            }}
+            onChange={inputChangeHandler}
             value={product.name}
+            invalid={!!formErrors.name}
           />
+          {formErrors.name && <FormFeedback> {formErrors.name} </FormFeedback>}
         </FormGroup>
 
         <FormGroup>
@@ -60,11 +127,11 @@ const CreateProduct = () => {
             id="product-img"
             name="img"
             type="text"
-            onChange={(event) => {
-              setProduct({ ...product, img: event.target.value });
-            }}
+            onChange={inputChangeHandler}
             value={product.img}
+            invalid={!!formErrors.img}
           />
+          {formErrors.img && <FormFeedback> {formErrors.img} </FormFeedback>}
         </FormGroup>
 
         <FormGroup>
@@ -73,11 +140,13 @@ const CreateProduct = () => {
             id="product-description"
             name="description"
             type="textarea"
-            onChange={(event) => {
-              setProduct({ ...product, description: event.target.value });
-            }}
+            onChange={inputChangeHandler}
             value={product.description}
+            invalid={!!formErrors.description}
           />
+          {formErrors.description && (
+            <FormFeedback> {formErrors.description} </FormFeedback>
+          )}
         </FormGroup>
 
         <FormGroup>
@@ -86,11 +155,13 @@ const CreateProduct = () => {
             id="product-price"
             name="price"
             type="number"
-            onChange={(event) => {
-              setProduct({ ...product, price: event.target.value });
-            }}
+            onChange={inputChangeHandler}
             value={product.price}
+            invalid={!!formErrors.price}
           />
+          {formErrors.price && (
+            <FormFeedback> {formErrors.price} </FormFeedback>
+          )}
         </FormGroup>
 
         <FormGroup>
@@ -99,14 +170,34 @@ const CreateProduct = () => {
             id="product-stock"
             name="stock"
             type="number"
-            onChange={(event) => {
-              setProduct({ ...product, stock: event.target.value });
-            }}
+            onChange={inputChangeHandler}
             value={product.stock}
+            invalid={!!formErrors.stock}
           />
+          {formErrors.stock && (
+            <FormFeedback> {formErrors.stock} </FormFeedback>
+          )}
         </FormGroup>
 
-        <Button type="submit">Save</Button>
+        <FormGroup>
+          <Label htmlFor="product-type">Product Type</Label>
+          <Input
+            id="product-type"
+            name="type"
+            type="select"
+            onChange={inputChangeHandler}
+            value={product.type}
+          >
+            <option value="accesories">Aksesuar</option>
+            <option value="home-furniture">Ev Mobilyası</option>
+            <option value="tech">Teknoloji</option>
+            <option value="clothes">Giyim</option>
+          </Input>
+        </FormGroup>
+
+        <Button color="primary" type="submit" disabled={!isFormValid}>
+          Save
+        </Button>
       </Form>
     </div>
   );
